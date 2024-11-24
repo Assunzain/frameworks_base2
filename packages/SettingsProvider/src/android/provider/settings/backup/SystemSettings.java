yaap/frameworks_base

base: Allow customizing default notification vibration pattern [1/2]
Re-use the patterns we've got for ringtone vibration as options
In addition allow crafting a custom pattern same as we do for ringtone

packages/SettingsProvider/src/android/provider/settings/backup/SystemSettings.java
@@ -156,5 +156,7 @@ public class SystemSettings {
         Settings.System.STATUS_BAR_BRIGHTNESS_CONTROL,
         Settings.System.SCREEN_OFF_FOD,
         Settings.System.STATUSBAR_CLOCK_POSITION,
+        Settings.System.NOTIFICATION_VIBRATION_PATTERN,
+        Settings.System.CUSTOM_NOTIFICATION_VIBRATION_PATTERN,
     };
 }

Assunzain/frameworks_base2

packages/SettingsProvider/src/android/provider/settings/backup/SystemSettings.java
@@ -120,7 +120,9 @@ public class SystemSettings {
                 Settings.System.STATUS_BAR_BATTERY_STYLE,
                 Settings.System.SHOW_BATTERY_PERCENT_INSIDE,
                 Settings.System.RINGTONE_VIBRATION_PATTERN,
-                Settings.System.CUSTOM_RINGTONE_VIBRATION_PATTERN
+                Settings.System.CUSTOM_RINGTONE_VIBRATION_PATTERN,
+                Settings.System.NOTIFICATION_VIBRATION_PATTERN,
+                Settings.System.CUSTOM_NOTIFICATION_VIBRATION_PATTERN
         ));
         if (Flags.backUpSmoothDisplayAndForcePeakRefreshRate()) {
             settings.add(Settings.System.PEAK_REFRESH_RATE);